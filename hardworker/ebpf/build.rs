49fcc8a8448f9975526eb5a961877f3df184329d
nan mu <mu.nan.11@outlook.com>
2025-04-07 16:43:55 +0900

    feat: 从toml文件导入环境变量并简化debug输出

--- hardworker/ebpf/build.rs
+++ hardworker/ebpf/build.rs
@@ -1,3 +1,7 @@
+use core::net::Ipv4Addr;
+use std::{env, fs, path::Path};
+use const_gen::{const_declaration, const_definition, CompileConst};
+use serde::Deserialize;
 use which::which;
 
 /// Building this crate has an undeclared dependency on the `bpf-linker` binary. This would be
@@ -11,7 +15,82 @@ use which::which;
 /// which would likely mean far too much cache invalidation.
 ///
 /// [bindeps]: https://doc.rust-lang.org/nightly/cargo/reference/unstable.html?highlight=feature#artifact-dependencies
+
 fn main() {
     let bpf_linker = which("bpf-linker").unwrap();
     println!("cargo:rerun-if-changed={}", bpf_linker.to_str().unwrap());
+
+    // 填充常量
+    let toml = std::fs::read_to_string("../../const.toml").unwrap();
+    // println!("{toml}");
+
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("const_gen.rs");
+
+    let consts: Consts = toml::from_str(&toml).unwrap();
+    let mac = Mac::from(consts.mac);
+    let ip = consts.ip;
+    let data = consts.data;
+
+    let const_declarations = vec![
+        "#[allow(unused)]".to_string(),
+        const_definition!(Mac),
+        "#[allow(unused)]".to_string(),
+        const_definition!(Ip),
+        "#[allow(unused)]".to_string(),
+        const_definition!(Data),
+
+        "#[allow(unused)]".to_string(),
+        const_declaration!(MAC = mac),
+        "#[allow(unused)]".to_string(),
+        const_declaration!(IP = ip),
+        "#[allow(unused)]".to_string(),
+        const_declaration!(DATA = data),
+    ].join("\n");
+
+    fs::write(&dest_path, const_declarations).unwrap();
+}
+
+#[derive(Deserialize)]
+struct Consts {
+    mac: MacToml,
+    ip: Ip,
+    data: Data,
+}
+
+#[derive(Deserialize)]
+struct MacToml {
+    logger: String,
+    hardware: String,
+    sensor: String,
+}
+
+impl From<MacToml> for Mac {
+    fn from(mac: MacToml) -> Self {
+        let logger = mac.logger.split(':').map(|s| u8::from_str_radix(s, 16).unwrap()).collect::<Vec<_>>().try_into().unwrap();
+        let hardware = mac.hardware.split(':').map(|s| u8::from_str_radix(s, 16).unwrap()).collect::<Vec<_>>().try_into().unwrap();
+        let sensor = mac.sensor.split(':').map(|s| u8::from_str_radix(s, 16).unwrap()).collect::<Vec<_>>().try_into().unwrap();
+        Self { logger, hardware, sensor }
+    }
+}
+
+#[derive(CompileConst)]
+struct Mac {
+    logger: [u8; 6],
+    hardware: [u8; 6],
+    sensor: [u8; 6],
+}
+
+#[derive(CompileConst, Deserialize)]
+struct Ip {
+    logger: Ipv4Addr,
+    hardware: Ipv4Addr,
+    sensor: Ipv4Addr,
+    tos: u8,
+}
+
+#[derive(CompileConst, Deserialize)]
+struct Data {
+    mtu: usize,
+    load_u64_count: usize,
 }
