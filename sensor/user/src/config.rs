c99580c02e111402330bedaf0235d65051d579c6
nan mu <mu.nan.11@outlook.com>
2025-04-16 18:53:10 +0900

    feat: 完成tcp客户端部分

---
+++ sensor/user/src/config.rs
@@ -0,0 +1,167 @@
+use anyhow::{Context, Result, anyhow};
+use log::debug;
+use serde::Deserialize;
+use std::cmp::Ordering;
+use std::fmt::Debug;
+use std::fs;
+use std::net::Ipv4Addr;
+use std::path::Path;
+use std::sync::Arc;
+use procfs::Current;
+
+mod consts {
+    //! const.toml 中的常量配置
+
+    use super::*;
+
+    #[derive(Debug, Clone, Deserialize)]
+    pub struct ConstConfig {
+        // pub mac: MacAddresses,
+        pub ip: IpAddresses,
+        pub mark: MarkConfig,
+        pub data: DataConfig,
+    }
+
+    /// MAC地址配置
+    // #[derive(Debug, Clone, Deserialize)]
+    // pub struct MacAddresses {
+    //     pub logger: String,
+    //     pub hardworker: String,
+    //     pub sensor: String,
+    // }
+
+    /// MAC地址配置解析后使用的结构体
+    // #[derive(Debug, Clone, Deserialize)]
+    // pub struct Mac {
+    //     pub logger: [u8; 6],
+    //     pub hardworker: [u8; 6],
+    //     pub sensor: [u8; 6],
+    // }
+
+    /// IP地址配置
+    #[derive(Debug, Clone, Deserialize)]
+    pub struct IpAddresses {
+        // pub logger: Ipv4Addr,
+        pub hardworker: Ipv4Addr,
+        // pub sensor: Ipv4Addr,
+    }
+
+    /// 数据包标记配置
+    #[derive(Debug, Clone, Deserialize)]
+    pub struct MarkConfig {
+        pub tos: u8,
+        pub port: u16,
+    }
+
+    /// 数据负载配置
+    #[derive(Debug, Clone, Deserialize)]
+    pub struct DataConfig {
+        pub mtu: usize,
+    }
+}
+
+/// config.toml 中的用户配置
+#[derive(Debug, Clone, Deserialize)]
+struct FileConfig {
+    pub tcp: Option<FileTcpConfig>,
+}
+
+/// TCP通信配置
+#[derive(Debug, Clone, Deserialize)]
+struct FileTcpConfig {
+    /// 网卡名称，可选
+    pub ifname: Option<String>,
+
+    /// 目标IP地址，可选，默认使用const.toml中的hardworker值
+    pub ip: Option<Ipv4Addr>,
+
+    /// 目标端口，可选，默认使用const.toml中的port值
+    pub port: Option<u16>,
+
+    /// TOS值，可选，默认使用const.toml中的tos值
+    pub tos: Option<u8>,
+
+    /// 数据包大小，可选，默认使用const.toml中的mtu值
+    pub size: Option<usize>,
+
+    /// 发送频率(Hz)，必须指定
+    pub freq: f64,
+}
+
+/// 最终合并的TCP配置
+#[derive(Debug, Clone)]
+pub struct TcpConfig {
+    pub ifname: Arc<str>,
+    pub target_ip: Ipv4Addr,
+    pub port: u16,
+    pub tos: u8,
+    pub size: usize,
+    pub freq: f64,
+}
+
+impl TryFrom<(FileConfig, consts::ConstConfig)> for TcpConfig {
+    type Error = anyhow::Error;
+    fn try_from((file_config, const_config): (FileConfig, consts::ConstConfig)) -> Result<Self> {
+        let tcp_config = file_config
+            .tcp
+            .ok_or_else(|| anyhow::anyhow!("TCP配置缺失"))?;
+
+        let target_ip = tcp_config.ip.unwrap_or(const_config.ip.hardworker);
+        let port = tcp_config.port.unwrap_or(const_config.mark.port);
+        let tos = tcp_config.tos.unwrap_or(const_config.mark.tos);
+        let size = tcp_config.size.unwrap_or(const_config.data.mtu);
+        let freq = tcp_config.freq;
+        let ifname = match tcp_config.ifname {
+            Some(ifname) => ifname,
+            None => {
+                let routers = procfs::net::RouteEntries::current()?;
+                let ip = target_ip.to_bits();
+                let best_router = routers
+                    .0
+                    .iter()
+                    .filter(|router| {
+                        let mask = router.mask.to_bits();
+                        let destination = router.destination.to_bits();
+                        ip & mask == destination & mask
+                    })
+                    .max_by(|&a, &b| {
+                        // 第一优先级：掩码中1的位数（最长前缀）
+                        let mask_cmp = a
+                            .mask
+                            .to_bits()
+                            .count_ones()
+                            .cmp(&b.mask.to_bits().count_ones());
+                        match mask_cmp {
+                            Ordering::Equal => b.metrics.cmp(&a.metrics),
+                            _ => mask_cmp,
+                        }
+                    })
+                    .ok_or(anyhow!("找不到对hardworker的路由, 检查目标ip或尝试手动填充config.toml的tcp.ifname字段"))?;
+                debug!("对ip: {ip} 找到路由: {}", best_router.iface);
+                best_router.iface.clone()
+            }
+        };
+
+        Ok(TcpConfig {
+            ifname: Arc::from(ifname),
+            target_ip,
+            port,
+            tos,
+            size,
+            freq,
+        })
+    }
+}
+
+impl TcpConfig {
+    /// path -> FileConfig&&ConstConfig -> TcpConfig
+    pub fn build<T: AsRef<Path> + Debug>(config_path: T, consts_path: T) -> Result<Self> {
+        let config = fs::read_to_string(&config_path)
+            .with_context(|| format!("输入config文件路径: {:?}", &config_path))?;
+        let config: FileConfig = toml::from_str(&config)?;
+        let consts = fs::read_to_string(&consts_path)
+            .with_context(|| format!("输入consts文件路径: {:?}", &consts_path))?;
+        let consts: consts::ConstConfig = toml::from_str(&consts)?;
+        Ok(Self::try_from((config, consts))?)
+    }
+}
--- sensor/user/src/main.rs
+++ sensor/user/src/main.rs
@@ -1,73 +1,59 @@
-use anyhow::Context as _;
-use aya::
-    programs::{Xdp, XdpFlags}
-;
 use clap::Parser;
-#[rustfmt::skip]
+use config::TcpConfig;
 use log::{debug, warn};
+use tcp::TcpHandler;
 use tokio::time::{sleep, Duration};
 
-// mod fd_handle;
+mod tcp;
+mod config;
+mod ebpf;
+
+use ebpf::EbpfBuilder;
+
 #[derive(Debug, Parser)]
 struct Opt {
-    #[clap(short, long, default_value = "wlan0")]
-    iface: String,
+    #[clap(short, long, default_value = "../config.toml")]
+    config: String,
+    #[clap(short, long, default_value = "../consts.toml")]
+    consts: String,
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let opt = Opt::parse();
+    let opt: Opt = Opt::parse();
+    let Opt { config, consts } = opt;
 
-    env_logger::init();
+    let config = TcpConfig::build(config, consts)?;
 
-    // Bump the memlock rlimit. This is needed for older kernels that don't use the
-    // new memcg based accounting, see https://lwn.net/Articles/837122/
-    let rlim = libc::rlimit {
-        rlim_cur: libc::RLIM_INFINITY,
-        rlim_max: libc::RLIM_INFINITY,
-    };
-    let ret = unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlim) };
-    if ret != 0 {
-        debug!("remove limit on locked memory failed, ret is: {}", ret);
-    }
+    let _ebpf = EbpfBuilder::build(config.ifname.clone())
+        .cancel_memlock()?
+        .build_xdp()?;
+    debug!("ebpf构建完成");
 
-    let mut ebpf = aya::Ebpf::load(aya::include_bytes_aligned!(concat!(
-        env!("OUT_DIR"),
-        "/sensor"
-    )))?;
-    if let Err(e) = aya_log::EbpfLogger::init(&mut ebpf) {
-        warn!("初始化ebpf日志器失败: {}", e);
-    }
-    let Opt { iface } = opt;
-    let program: &mut Xdp = ebpf.program_mut("sensor").unwrap().try_into()?;
-    program.load()?;
-    program
-        .attach(&iface, XdpFlags::default())
-        .context("默认flag连接xdp失败，考虑特定flag")?;
-
-    println!("主进程PID: {}", std::process::id());
-    println!("主线程TID: {}", unsafe {
-        libc::syscall(libc::SYS_gettid)
-    });
+    let tcp_client = TcpHandler::from(config);
+    let tcp_shutdown_tx = tcp_client.get_signal();
+    
+    // 启动TCP客户端
+    tcp_client.start().await?;
+    debug!("TCP客户端启动完成");
 
     let sig_int = tokio::signal::ctrl_c();
-
     println!("准备完成，等待Ctrl-C或超时退出...");
 
     tokio::select! {
         _ = sig_int => {
             println!("\nCtrl+c退出...");
-            // shutdown
-            //     .send(())
-            //     .expect("发送关闭信号失败，考虑子线程出错或外部干预，考虑sudo kill主线程");
         }
         _ = sleep(Duration::from_secs(1000)) => {
             println!("\n超时退出...");
-            // shutdown
-            //     .send(())
-            //     .expect("发送关闭信号失败，考虑子线程出错或外部干预，考虑sudo kill主线程");
         }
     }
 
+    // 发送关闭信号给TCP客户端
+    if let Err(e) = tcp_shutdown_tx.send(()).await {
+        warn!("发送TCP关闭信号失败: {:?}", e);
+    }
+
+    println!("程序退出");
     Ok(())
 }
